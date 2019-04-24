7bc996b4920423c7caa333b87c20b32f5ac2e606
Nil <nilbesombes@gmail.com>
2019-04-24 13:29:03 +0200

    fixed strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -103,6 +103,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :character_id, :episode, :user_id, :status)
+      params.require(:post).permit(:title, :character_id, :episode, :user_id, :status, :point)
     end
 end
